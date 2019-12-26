47e79bb265e3faeb0fe1fe107c9f15234c79d3ba
heartbeats1216@gmail.com <Heartbeats1216@gmail.com>
2019-12-27 00:28:22 +0800

    Python Code for 1.1

---
+++ Introduction/Greatest Common Divisor.py
@@ -0,0 +1,18 @@
+
+"""
+-*- coding: utf-8 -*-
+@author: Hongzhi Fu
+"""
+
+# Euclid's algorithm to find the greatest common divisor with the complexity O(logn)
+
+def gcd(m, n):
+    while n != 0:
+        r = m
+        m = n
+        n = r % n
+        gcd(m, n)
+    return m
+
+m = 24; n = 16
+print("The greatest common divisor of {} and {} is {}".format(m, n, gcd(m, n)))
