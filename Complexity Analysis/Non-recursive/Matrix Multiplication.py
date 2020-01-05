79cdcdddd77cb7443cdd473b7d95e65dd4a1351f
heartbeats1216@gmail.com <Heartbeats1216@gmail.com>
2020-01-06 00:24:34 +0800

    Python Code for 3.2

---
+++ Complexity Analysis/Non-recursive/Matrix Multiplication.py
@@ -0,0 +1,23 @@
+"""
+# -*- coding: utf-8 -*-
+@author: Hongzhi Fu
+"""
+
+# implementation of matrix multiplication (2 × 2 square matrix)
+
+
+def matmul(A, B):
+    n = len(A)
+    C = [[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                C[i][j] = C[i][j] + A[i][k] * B[k][j]
+    return C
+
+
+A = [[-3, 1], [2, 5]]
+B = [[5, 3], [7, -3]]
+print("A = \n", A)
+print("B = \n", B)
+print("AB = \n", matmul(A, B))
