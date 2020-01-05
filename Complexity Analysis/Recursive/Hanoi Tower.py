037806c91f6e309cd4750befb8c2fe45581cd69b
heartbeats1216@gmail.com <Heartbeats1216@gmail.com>
2020-01-06 00:24:48 +0800

    Python Code for 3.3

---
+++ Complexity Analysis/Recursive/Hanoi Tower.py
@@ -0,0 +1,19 @@
+"""
+# -*- coding: utf-8 -*-
+@author: Hongzhi Fu
+"""
+
+# implementation of Hanoi Tower (recursive)
+
+
+def hanoi(a, b, c, n):
+    if n == 1:
+        print("{} -> {}".format(a, c))
+        return
+    hanoi(a, c, b, n-1)  # rod a as source, rod b as target, rod c as pivot
+    hanoi(a, b, c, 1)  # set the number of disk to be moved to 1
+    hanoi(b, a, c, n-1)  # rod b as source, rod c as target, rod a as pivot
+
+
+print("Solution of Hanoi Tower with 4 disks: ", )
+hanoi('a', 'b', 'c', 3)
