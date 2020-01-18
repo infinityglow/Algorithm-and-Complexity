67b5ef42e4479318865aab2892aeea64581ebe00
heartbeats1216@gmail.com <Heartbeats1216@gmail.com>
2020-01-18 17:13:37 +0800

    Python Code for 4.1

--- Brute Force/Bubble Selection Sort/selection sort.py
+++ Brute Force/Bubble Selection Sort/selection sort.py
@@ -5,7 +5,7 @@ import time
 # implementation of selection sort
 # to sort a random array with 10,000 elements
 # two metrics ("number of comparison" and "consumed time") for efficiency evaluation
-# time complexity: Θ(n^2)
+# time complexity: Θ(n^2) in all cases
 # space complexity Θ(1)
 # stability: unstable
 
@@ -23,15 +23,16 @@ def selection_sort(array):
 
 
 time_in_total = 0
-epoch = 10  # num of iteration
+epoch = 5  # num of iteration
 total_comparison = 0
+
 for i in range(epoch):
     time_start = time.time()
     array = [random.randint(0,10000) for i in range(10000)]
     comparison = selection_sort(array)
     time_finish = time.time()
     total_comparison += comparison
-    time_in_total += time_finish-time_start
-    print("Epoch {}: \n  number of comparison: {}\n  time consumed: {:.2f} s".format(i+1, comparison, time_finish-time_start))
-print("average number of comparison: %d" % (total_comparison/10))
-print('average time consumed: {:.2f} s'.format(time_in_total/10))
+    time_in_total += time_finish - time_start
+    print("Epoch {}: \n  number of comparison: {}\n  time consumed: {:.4f} s".format(i+1, comparison, time_finish-time_start))
+print("average number of comparison: %d" % (total_comparison/epoch))
+print('average time consumed: {:.4f} s'.format(time_in_total/epoch))
